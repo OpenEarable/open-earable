3c97c626789ab3fef943850eea9409e39a1131c4
Xraydylan <xraydylan@gmail.com>
2022-07-29 07:48:29 +0200

    PDM MEMES

---
+++ EarableMEMS/PDM2.h
@@ -0,0 +1,54 @@
+#ifndef _PDM2_H_INCLUDED
+#define _PDM2_H_INCLUDED
+
+#include <Arduino.h>
+#include <pinDefinitions.h>
+
+#include "PDMDoubleBuffer.h"
+
+class PDMClass2
+{
+public:
+  PDMClass2();
+  virtual ~PDMClass2();
+
+  void init(int dinPin, int clkPin, int pwrPin);
+  int begin(int channels, int sampleRate);
+  void end();
+
+  virtual int available();
+  virtual int read(void* buffer, size_t size);
+
+  void onReceive(void(*)(void));
+
+  //PORTENTA_H7 min -12 max 51
+  //NANO 33 BLE SENSe min 0 max 80
+  //NICLA_VISION min 0 max 8
+  void setGain(int gain);
+  void setBufferSize(int bufferSize);
+  size_t getBufferSize();
+
+// private:
+  void IrqHandler(bool halftranfer);
+
+private:
+  int _dinPin;
+  int _clkPin;
+  int _pwrPin;
+
+  int _channels;
+  int _samplerate;
+
+  int _gain;
+  int _init;
+
+  int _cutSamples;
+
+  PDMDoubleBuffer _doubleBuffer;
+
+  void (*_onReceive)(void);
+};
+
+extern PDMClass2 PDM2;
+
+#endif
