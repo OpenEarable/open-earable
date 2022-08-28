433a50884968bf02e7f548ec6bb3d15e9a3f0ed9
Xraydylan <xraydylan@gmail.com>
2022-08-28 22:22:40 +0200

    debug fix

--- earable_web/earable_dashboard/Flask_Dashboard.py
+++ earable_web/earable_dashboard/Flask_Dashboard.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template
 
 host = "localhost"
-
-#host = "10.12.181.86"
-#host = "10.10.10.10"
 port = 5002
 
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
@@ -13,4 +10,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port, debug=False)
